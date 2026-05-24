4fef977ce196a18a565363696fc9cc295867fb5c
DayronCasas <dayron.casas.sanchez@vallegrande.edu.pe>
2026-05-23 23:40:37 -0500

    feat: add correction

--- src/test/java/ap1/dayron/casas/SpringWebfluxNosqlApplicationTests.java
+++ src/test/java/ap1/dayron/casas/SpringWebfluxNosqlApplicationTests.java
@@ -1,10 +1,19 @@
 package ap1.dayron.casas;
 
+import ap1.dayron.casas.repository.EmailVerificationRepository;
+import ap1.dayron.casas.repository.TranslationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class SpringWebfluxNosqlApplicationTests {
+
+	@MockBean
+	private TranslationRepository translationRepository;
+
+	@MockBean
+	private EmailVerificationRepository emailVerificationRepository;
 
 	@Test
 	void contextLoads() {
